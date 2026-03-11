2f467008414f7861a19a901b986e6cd9e49d3d62
shivani <shivaniavanikkarasi@gmail.com>
2026-03-11 14:36:56 +0530

    [Shivani] : Added : UC4 code

--- helloapp/src/main/java/com/bridgelabz/helloapp/HelloController.java
+++ helloapp/src/main/java/com/bridgelabz/helloapp/HelloController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.helloapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,11 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
+        return "Hello from BridgeLabz";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost() {
         return "Hello from BridgeLabz";
     }
 }
