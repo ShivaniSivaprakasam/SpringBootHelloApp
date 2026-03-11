b0baa231e6f9b3193af589a54b0f84ae14347575
shivani <shivaniavanikkarasi@gmail.com>
2026-03-11 11:51:19 +0530

    [Shivani] : Added : UC3 REST Hello API

--- helloapp/src/main/java/com/bridgelabz/helloapp/HelloController.java
+++ helloapp/src/main/java/com/bridgelabz/helloapp/HelloController.java
@@ -1,15 +1,13 @@
 package com.bridgelabz.helloapp;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello from BridgeLabz");
-        return "hello";
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from BridgeLabz";
     }
 }
